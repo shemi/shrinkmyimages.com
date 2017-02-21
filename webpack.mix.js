27097c7000aad59dddeadc7e1ff1e4730ee3e90a
shemi <shemi.perez@gmail.com>
2017-02-21 22:48:49 +0200

    add upload button info

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,8 +14,35 @@ const {mix} = require('laravel-mix');
 mix.js('resources/assets/js/app.js', 'public/js')
     .sass('resources/assets/sass/app.scss', 'public/css')
     .autoload({})
-    .browserSync('shrinkmyimages.dev');
+    .sourceMaps()
+    .browserSync({
+        proxy: 'shrinkmyimages.dev'
+    });
+
+mix.options({
+    extractVueStyles: true
+});
 
 if (mix.config.inProduction) {
     mix.version();
 }
+
+// Full API
+// mix.js(src, output);
+// mix.extract(vendorLibs);
+// mix.sass(src, output);
+// mix.less(src, output);
+// mix.browserSync('my-site.dev');
+// mix.combine(files, destination);
+// mix.copy(from, to);
+// mix.minify(file);
+// mix.sourceMaps(); // Enable sourcemaps
+// mix.version(); // Enable versioning.
+// mix.disableNotifications();
+// mix.setPublicPath('path/to/public');
+// mix.autoload({}); <-- Will be passed to Webpack's ProvidePlugin.
+// mix.webpackConfig({}); <-- Override webpack.config.js, without editing the file directly.
+// mix.then(function () {}) <-- Will be triggered each time Webpack finishes building.
+// mix.options({
+//   extractVueStyles: false // default
+// });
